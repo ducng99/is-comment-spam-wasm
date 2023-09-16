43299b62d7a92b1ebc7a6c87d51dba72a3f5848f
ducng99 <tom@ducng.dev>
2023-09-16 19:19:27 +1200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/ducng99/is-comment-spam-wasm
+
+go 1.21.1
+
+require github.com/ducng99/is-comment-spam v1.0.0
+
+replace github.com/ducng99/is-comment-spam => ../is-comment-spam
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"syscall/js"
+
+	spam "github.com/ducng99/is-comment-spam"
+)
+
+func main() {
+	js.Global().Set("isCommentSpam", js.FuncOf(func(this js.Value, args []js.Value) any {
+		if len(args) != 1 {
+			return false
+		}
+
+		return spam.IsSpam(args[0].String())
+	}))
+}
